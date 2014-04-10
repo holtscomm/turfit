be91f49435271b95e0e6052a882a0e1f771c123d
Graham Holtslander <menello@gmail.com>
2014-04-10 01:12:55 -0600

    More tweaks

---
+++ donors/includes/config.php
@@ -0,0 +1,23 @@
+<?php
+if(!defined('IN_BLOG'))
+{
+	exit;
+}
+
+$sqlconfig = array();
+if(strstr($_SERVER["SERVER_NAME"], 'localhost'))
+{
+    $sqlconfig['username'] = 'root'; // database username
+    $sqlconfig['password'] = 'rootroot'; // database password
+    $sqlconfig['host'] = '127.0.0.1'; // database host (usually localhost)
+    $sqlconfig['dbname'] = 'minidonorblog'; // database name
+}
+else if(strstr($_SERVER["SERVER_NAME"], 'turfit.ca'))
+{
+    $sqlconfig['username'] = 'turfit_miniblog'; // database username
+    $sqlconfig['password'] = '1super!mini@'; // database password
+    $sqlconfig['host'] = 'mysql1006.blacksun.ca'; // database host (usually localhost)
+    $sqlconfig['dbname'] = 'turfit_minidonorblog'; // database name
+}
+
+?>
